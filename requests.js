d318f658b9bb80a7e4ba433f6bc42df2c35cec20
Jorgen <jorgen.cani7@outlook.com>
2022-05-11 18:43:29 +0300

    v0.2

--- requests.js
+++ requests.js
@@ -1,13 +1,50 @@
 // Requests using fetch
+function createTable(title, author, genre, price) {
+    let table = document.createElement('table')
+    const data = [title, author, genre, price]
+    const label_names = ['Title', 'Author', 'Genre', 'Price']
+
+    for (let i = 0; i < data.length; i++) {
+        let td_label = document.createElement('td')
+        let label = document.createElement('label')
+        label.textContent = label_names[i]
+        td_label.appendChild(label)
+
+        let td_data_text = document.createElement('td')
+        td_data_text.textContent = data[i]
+
+        let tr = document.createElement('tr')
+        tr.appendChild(td_label)
+        tr.appendChild(td_data_text)
+
+        table.appendChild(tr)
+    }
+    return table
+}
+
 async function getData () {
     const list = document.getElementById('result-list')
-    fetch('http://localhost:8080/books')
+    fetch('http://localhost:8080/books/search')
         .then(response => response.json())
         .then(data => {
             for (const book of data) {
+                let div = document.createElement('div');
+                div.className = 'book-result'
+
+                div.appendChild(createTable(book.TITLE, book.AUTHOR, book.GENRE, book.PRICE))
+                // div.appendChild(document.createElement('strong'))
+                //     .textContent = book.TITLE;
+                // div.appendChild(document.createElement('strong'))
+                //     .textContent = book.AUTHOR;
+                // div.appendChild(document.createElement('strong'))
+                //     .textContent = book.GENRE;
+                // div.appendChild(document.createElement('strong'))
+                //     .textContent = book.PRICE;
+
                 let li = document.createElement('li');
-                li.appendChild(document.createElement('strong'))
-                    .textContent = book.TITLE;
+                li.style = 'list-style: none;'
+                li.appendChild(div)
+
                 list.appendChild(li)
             }
         })
--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const port = 8080
 const bodyParser = require('body-parser')
 // Cannot use requests.js because of cors, installing it
 const cors = require('cors')
+let {response} = require("express");
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// GET
-app.get('/books', async (req, res) => {
+// GET (search)
+app.get('/books/search', async (req, res) => {
     try {
         const result = await db.pool.query("select * from books");
         res.send(result)
@@ -23,4 +24,27 @@ app.get('/books', async (req, res) => {
     }
 });
 
+// POST (insert)
+app.post('/books/', async (req, res) => {
+    // TODO: Try-Catch block
+    // console.log(req)
+    response = {
+        author: req.body.author,
+        title:  req.body.title,
+        genre:  req.body.genre,
+        price:  req.body.price
+    };
+
+    console.log(response)
+    res.end(JSON.stringify(response));
+    try {
+        const result = await db.pool.query(`INSERT INTO books (AUTHOR, TITLE, GENRE, PRICE) VALUE ('${response.author.toString()}', '${response.title.toString()}', '${response.genre.toString()}', '${parseFloat(response.price)}')`);
+        // const result = await db.pool.query("INSERT INTO Library.books (AUTHOR, TITLE, GENRE, PRICE) VALUE ('LOL', 'LOL', 'LOL', 123.2)")
+        res.send(result)
+    } catch (err) {
+        throw err;
+    }
+});
+
+// Used for insertions
 app.listen(port, () => console.log(`Listening on port ${port}`));
