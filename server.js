6d53554c98f20122264ab9c5b29db15dd9d69dc3
Jorgen <jorgen.cani7@outlook.com>
2022-05-13 01:24:22 +0300

    Backend Changes
    - The site now uses the 'public' directory as a path.
    - GET request for book searching works

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 // Code from https://mariadb.com/resources/blog/getting-started-with-connector-node-js/
 // Express seems to be an HTTP application, I am going to use AJAX, not express
 
-const express = require('express')
-const db = require('./db')
-const app = express()
-const port = 8080
-const bodyParser = require('body-parser')
-// Cannot use requests.js because of cors, installing it
-const cors = require('cors')
-let {response} = require("express");
+// Requirements
+const express = require('express');
+const db = require('./db');
+const cors = require('cors');
 
-app.use(cors())
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+// Cannot use requests.js because of cors, installing it
+
+let {response} = require("express");
+const path = require("path");
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-// GET (search)
+// GET for Searching books
 app.get('/books/search', async (req, res) => {
     try {
-        const result = await db.pool.query("select * from books");
+        const query = req.query.q;
+        console.log(`User searched for ${query}`);
+        const result = await db.pool.query(`SELECT books.TITLE, books.AUTHOR, books.GENRE, books.PRICE FROM Library.books WHERE TITLE LIKE '${query}%'`);
         res.send(result)
     } catch (err) {
         throw err;
     }
 });
 
-// POST (insert)
+// GET for fetching all books (TEMP)
+app.get('/books/all', async (req, res) => {
+    try {
+        const result = await db.pool.query("SELECT * FROM Library.books");
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+
+// POST for inserting books
 app.post('/books/', async (req, res) => {
     // TODO: Try-Catch block
-    // console.log(req)
+    console.log(req);
     response = {
         author: req.body.author,
         title:  req.body.title,
@@ -35,16 +51,15 @@ app.post('/books/', async (req, res) => {
         price:  req.body.price
     };
 
-    console.log(response)
+    console.log(response);
     res.end(JSON.stringify(response));
     try {
-        const result = await db.pool.query(`INSERT INTO books (AUTHOR, TITLE, GENRE, PRICE) VALUE ('${response.author.toString()}', '${response.title.toString()}', '${response.genre.toString()}', '${parseFloat(response.price)}')`);
-        // const result = await db.pool.query("INSERT INTO Library.books (AUTHOR, TITLE, GENRE, PRICE) VALUE ('LOL', 'LOL', 'LOL', 123.2)")
-        res.send(result)
+        await db.pool.query(`INSERT INTO books (AUTHOR, TITLE, GENRE, PRICE) VALUE ('${response.author.toString()}', '${response.title.toString()}', '${response.genre.toString()}', '${parseFloat(response.price)}')`);
+        // res.send(result)
     } catch (err) {
         throw err;
     }
 });
 
-// Used for insertions
+// Listing to 8080 port
 app.listen(port, () => console.log(`Listening on port ${port}`));
